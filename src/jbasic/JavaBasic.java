0c7867c77d5abc1fb12357ceb5d3fea8c012f65f
suganesh <suganeshvaratharajan@gmail.com>
2023-05-11 13:04:31 +0530

    Basic Java Program

---
+++ src/jbasic/JavaBasic.java
@@ -0,0 +1,13 @@
+package jbasic;
+
+public class JavaBasic {
+public static void main(String[] args) {
+		
+		
+		System.out.println("Basic Java Program");
+		
+
+	}
+
+
+}
---
+++ src/jbasic/package-info.java
@@ -0,0 +1,1 @@
+package jbasic;
