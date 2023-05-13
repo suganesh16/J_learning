340bb0ffdd81fdd511c66b3c52e6869d6490e2d2
suganesh <suganeshvaratharajan@gmail.com>
2023-05-13 10:43:29 +0530

    variables and datatype

---
+++ src/jbasic/Variables.java
@@ -0,0 +1,37 @@
+package jbasic;
+
+public class Variables {
+	
+	public static void main(String args[]) {
+		
+      int Rollno=10017;
+      System.out.println(Rollno);
+		
+      String name= "Suganesh";
+      System.out.println(name);
+      
+      char gender='M';
+      System.out.println(gender);
+      
+      short sno=17;
+      System.out.println(sno);
+        
+      byte age= 23;
+      System.out.println(age);
+      
+      
+      long Mobileno=9800000000l;
+      System.out.println(Mobileno);
+      
+      float percentage=6.8f;
+      System.out.println(percentage);
+      
+      boolean b=true;
+      System.out.println(b);
+      
+      
+    		  
+		
+		
+}
+}
