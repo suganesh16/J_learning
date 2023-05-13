b7572723f9699aba5219114e401cba58b82f3fc3
suganesh <suganeshvaratharajan@gmail.com>
2023-05-13 11:06:12 +0530

    Addiction

---
+++ src/jbasic/Addiction.java
@@ -0,0 +1,22 @@
+package jbasic;
+
+public class Addiction {
+	
+	public static void main(String args[]) {
+		
+		int a= 10;
+		int b=39;
+		
+		int c=a+b;
+		
+		
+		System.out.println(c);
+		
+		
+		
+		
+		
+		
+		
+	}
+}
